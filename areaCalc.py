07cdd25855e314b126f75d92467a604ce60eb1e9
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-17 21:12:40 +0530

    Done 3 Exercises

---
+++ areaCalc.py
@@ -0,0 +1,6 @@
+length = int(input("Enter the length of a rectangle: "))
+width = float(input ("Enter the width of a rectangle: "))
+height = float(input("Enter the height of a rectangle: "))
+area = length * width * height
+
+print(f"The area is: {area}cm^3")
