6d68588315cb0dd9bc61f1ef659a89983e247281
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-19 21:41:23 +0530

    Add formatSpecifier.py

---
+++ formatSpecifier.py
@@ -0,0 +1,19 @@
+# format specifiers = {value:flags} format a value based on what flags are inserted
+
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) = allocate that many spaces
+# :03 = allocate and zero pad that many spaces
+# :< = left justify
+# :> = right justify
+# :^ = center align
+# :+ = use a plus sign to indicate positive value
+# : = insert a space before positive numbers
+# :, = comma separator
+
+price1 = 30000.14159
+price2 = -987.65
+price3 = 12.34
+
+print(f"Price 1 is ${price1:+,.2f}")
+print(f"Price 2 is ${price2:+}")
+print(f"Price 3 is ${price3:-}")
