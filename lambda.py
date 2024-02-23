e50e96222485ad63b1901db84121e5fcc3300d28
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-02-23 14:59:45 +0530

    Add lambda.py

---
+++ lambda.py
@@ -0,0 +1,21 @@
+# lambda function = function written in 1 line using lambda keyword
+#                   accepts any number of arguments, but only has one expression.
+#                   (think of it as a shortcut)
+#                   (useful if needed for a short period of time, throw-away)
+#
+# lambda parameters:expression
+
+# def double(x):
+#     return x*2
+# print(double(2))
+
+double = lambda x:x * 2
+multiply = lambda x,y : x * y
+add = lambda x,y,z : x+y+z
+full_name = lambda first_name,last_name:first_name + " " + last_name
+age_check = lambda age:True if age >=18 else False
+print(double(5))
+print(multiply(2,3))
+print(add(3,5,2))
+print(full_name("Acacia","Morningstar"))
+print(age_check(31))
